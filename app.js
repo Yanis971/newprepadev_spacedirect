f02acaa439fd2053be24e97d01bebdce64d48c5e
Yanis971 <yanisgeranhachon@gmail.com>
2024-02-26 09:33:03 +0100

    init project express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// route d'acceuil
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+// ecoute du serveur sur le port 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log('Serveur is running on port http://localhost:$(PORT)');
+})
