1434f835f1b4d0cb44e8b8dab7d9681dd9ec5302
Yanis971 <yanisgeranhachon@gmail.com>
2024-02-27 09:21:27 +0100

    add auth controller

--- controller/authController.js
+++ controller/authController.js
@@ -1,7 +1,7 @@
 const User = require('../model/userScheme');
 const bcrypt = require('bcrypt');
 
-// Affiche le formulaire d'inscription
-exports.showregistrationForm = (req, res) => {
+//Affiche le formulaire d'inscription
+exports.showRegistrationForm = (req, res) => {
     res.render('register', { error: null });
 }
